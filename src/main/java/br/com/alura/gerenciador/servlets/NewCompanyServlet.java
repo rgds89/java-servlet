1be6a15174c86fb709a1891ce6de1993a18bb145
Roger Silva <rogersilvati@gmail.com>
2022-05-12 15:28:01 -0300

    Created servlet and html form

---
+++ src/main/java/br/com/alura/gerenciador/servlets/NewCompanyServlet.java
@@ -0,0 +1,23 @@
+package br.com.alura.gerenciador.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/newCompany")
+public class NewCompanyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("nameCompany");
+		out.println("<html><body>Empresa " + name + " cadastrada com sucesso</body></html>");
+	}
+
+}
