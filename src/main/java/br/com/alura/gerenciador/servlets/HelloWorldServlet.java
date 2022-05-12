a5d4571e896cd7b65a2848f867ca3a7dd9a10658
Roger Silva <rogersilvati@gmail.com>
2022-05-12 12:33:35 -0300

    First Servlet

---
+++ src/main/java/br/com/alura/gerenciador/servlets/HelloWorldServlet.java
@@ -0,0 +1,27 @@
+package br.com.alura.gerenciador.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/hello")
+public class HelloWorldServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Hello world, congratulations you created your first servlets");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
