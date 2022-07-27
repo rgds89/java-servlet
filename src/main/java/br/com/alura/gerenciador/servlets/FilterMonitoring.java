4f66d7e90854c80d5542742d72d33a7f4b146efe
Roger Silva <rogersilvati@gmail.com>
2022-07-27 11:28:16 -0300

    Created monitoring filter

---
+++ src/main/java/br/com/alura/gerenciador/servlets/FilterMonitoring.java
@@ -0,0 +1,31 @@
+package br.com.alura.gerenciador.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter(urlPatterns = "/control")
+public class FilterMonitoring implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		Long before = System.currentTimeMillis();
+		
+		//Action execution
+		chain.doFilter(request, response);
+		
+		Long after = System.currentTimeMillis();
+		
+		System.out.println("Tempo de execução:" + (after - before));
+		
+		
+	}
+
+}
